6c4209b5c49d034f6f27700124cc8e0991741163
Joren Vrancken <jorenvrancken@gmail.com>
2024-07-21 20:50:16 +0200

    Add documentation

--- sample_finder/sources/__init__.py
+++ sample_finder/sources/__init__.py
@@ -1,6 +1,6 @@
 from sample_finder.sources.malpedia import SourceMalpedia
 from sample_finder.sources.malshare import SourceMalshare
-from sample_finder.sources.malware_bazaar import SourceMalwareBazaar
+from sample_finder.sources.malwarebazaar import SourceMalwareBazaar
 from sample_finder.sources.source import Source
 from sample_finder.sources.virusshare import SourceVirusshare
 from sample_finder.sources.virustotal import SourceVirustotal
